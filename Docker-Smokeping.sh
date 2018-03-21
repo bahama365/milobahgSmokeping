3e2e71cc9f4aaa00e75f124e4ede7f8d7af4cb0d
bahama365 <31001392+bahama365@users.noreply.github.com>
2018-03-21 17:12:58 +0000

    Add files via upload

---
+++ Docker-Smokeping.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+target() {
+  rand=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+  read -p "What host/IPi do you want to ping?: " target1
+  echo "You want toi ping: $target1"
+  echo >> $PWD/config.d/Targets
+  echo -e "++ $rand" >> $PWD/config.d/Targets
+  echo >> $PWD/config.d/Targets
+  echo "menu = $target1" >> $PWD/config.d/Targets
+  echo "title = $target1" >> $PWD/config.d/Targets
+  echo "host = $target1" >> $PWD/config.d/Targets
+  echo >> $PWD/config.d/Targets
+  read -p "Do you want to add any more targets? (y/n) " answer
+}
+
+echo
+echo  "This script will customise and then build a new Smokeping container"
+echo
+sleep 1
+echo  "Applying customisation..."
+echo
+target
+  while [ $answer == "y" ]; do
+    target
+  done
+echo 
+echo "Updating $PWD/milobahgSmokeping/config.d/Targets..."
+sleep 1
+echo "...done!"
+sleep 1
+echo "Building image..."
+docker build -t smokeping $PWD/milobahgSmokeping
+echo 
+sleep 1
+read -p "Image built, what do you want to call the container?: " containerName
+read -p "What port do you want to access Smokeping on?: " port
+docker run -d -p $port:80 --name $containerName --rm smokeping
+echo
+echo "Container is running, access from your host on: 
+http://dockerHostIP/smokeping/smokeping.cgi"
